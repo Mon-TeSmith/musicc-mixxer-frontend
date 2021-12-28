4033fa4b199f7e5088dcfe12f564debf34e66090
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-12-28 10:23:17 -0600

    modified signup page with react

--- src/Components/SignupPage/SignupPage.jsx
+++ src/Components/SignupPage/SignupPage.jsx
@@ -4,47 +4,78 @@ import "./SignupPage.css"
 import FormInput from '../FormInput';
 import { LocalLaundryService } from '@mui/icons-material';
 
-    const{username, setUsername}= useState("");
-    const{email, setEmail}= useState("");
-    const{firstName, setFirstName}= useState("");
-    const{lastName, setLastName}= useState("");
-    // const usernameRef = useRef()
-    
-    
+    const SignupPage =() => {
+        const [values, setValues] = useState({
+            username:"",
+            email:"",
+            birthday:"",
+            password:"",
+            confirmPassword:"",
+        });
 
+        const inputs = [
+            {
+                id:1,
+                name:"username",
+                type:"text",
+                placeholder:"Username",
+                label:"Username"
+            },
+            {
+                id:2,
+                name:"Email",
+                type:"text",
+                placeholder:"Email",
+                label:"Email"
+            },
+            {
+                id:3,
+                name:"Birthday",
+                type:"text",
+                placeholder:"Birthday",
+                label:"Birthday"
+            },
+            {
+                id:4,
+                name:"password",
+                type:"password",
+                placeholder:"password",
+                label:"password"
+            },
+            {
+                id:5,
+                name:"confirmPassword",
+                type:"passwprd",
+                placeholder:"confirmPassword",
+                label:"confirmPassword"
+            }
+        ];
+
+        let response = await axios.post(`http://localhost:3000/api/users/login`, getUser);
+        console.log(response.data);
+        localStorage.setItem('token', response.data)
+        window.location ='/';
+  
+       
+    
     console.log("re-rendered");
     
-    const handlesubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(userNameRef);
-
-    }
-        preventDefault();
-
-        // const getUser= {
-        //     username:username,
-        //     email:email,
-        //     firstName:firstName,
-        //     lastName:lastName,   
-        // };
-        
-        // let response = await axios.post(`http://localhost:3000/api/users/login`, getUser);
-        // console.log(response.data);
-        // localStorage.setItem('token', response.data)
-        // window.location ='/';
+    };  
+   
+        return (
+            <div className="app"> 
+                <form onSubmit={handleSubmit}> 
+                    {inputs.map((input) => (
+                        <FormInput key={input.id} {...input} />
+                    ))}
+                    <button>Submit</button>
+                    {/* <img className="bg-image" src= alt="" /> */} 
+                </form>         
+            </div>
+    );  
+ };
   
-    return (
-        <div className="app"> 
-            <form onSubmit={handleSubmit}> 
-                <FormInput refer={usernameRef} />
-                <FormInput placeholder="Email:" setEmail={setEmail}/>
-                <FormInput placeholder="FirstName:" setFirstName={setFirstName}/>
-                <FormInput placeholder="LastName:" setLastName={setLastName}/>
-                <button>Submit</button>
-                {/* <img className="bg-image" src= alt="" /> */} 
-            </form>         
-        </div>
-    );   
- }
-
+        
  export default SignupPage;
